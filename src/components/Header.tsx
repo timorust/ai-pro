68bad94576fb11775dfa6ccd24dbc69ee4d43bdf
tamir.rustamov <timorust1@gmail.com>
2025-05-26 14:34:05 +0300

    Sign Up-Login-button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,22 @@
-import { shadow } from '@/styles/utils'
+import { shadow } from "@/styles/utils";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { Button } from "./ui/button";
+// import DarkModeToggle from "./DarkModeToggle";
+// import LogOutButton from "./LogOutButton";
+// import { getUser } from "@/auth/server";
 
-function Header() {
+async function Header() {
+  const user = null;
   return (
-    <header className="bg-popover relative flex h-24 w-full items-center justify-between px-3 sm:px-8" style={{
-			boxShadow: shadow,
-		}}>
-      <Link href="/">
+    <header
+      className="bg-popover relative flex h-24 w-full items-center justify-between px-3 sm:px-8"
+      style={{
+        boxShadow: shadow,
+      }}
+    >
+      <Link className="flex items-end gap-2" href="/">
         <Image
           src="/logodolphin.png"
           alt="logo"
@@ -17,7 +25,29 @@ function Header() {
           className="rounded-full"
           priority
         />
+
+        <h1 className="flex flex-col pb-1 text-2xl leading-6 font-semibold">
+          AI <span>Notes</span>
+        </h1>
       </Link>
+      <div className="flex gap-4">
+        {user ? (
+          // <LogOutButton />
+          "Logout"
+        ) : (
+          <>
+            <Button asChild>
+              <Link href="/sign-up" className="hidden sm:block">
+                Sign Up
+              </Link>
+            </Button>
+            <Button asChild variant="outline">
+              <Link href="/login">Login</Link>
+            </Button>
+          </>
+        )}
+        {/* <DarkModeToggle /> */}
+      </div>
     </header>
   );
 }
