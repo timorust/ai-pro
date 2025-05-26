e75116699e44ac64d25c38164fdb5aa67a09f229
tamir.rustamov <timorust1@gmail.com>
2025-05-26 14:16:40 +0300

    shadow

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,22 @@
+import { shadow } from '@/styles/utils'
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 function Header() {
   return (
-    <header>
+    <header className="bg-popover relative flex h-24 w-full items-center justify-between px-3 sm:px-8" style={{
+			boxShadow: shadow,
+		}}>
       <Link href="/">
-        <Image 
-				src="/logodolphin.png" 
-				alt="logo" 
-				height={60}
-				width={60}
-				className='rounded-full'
-				priority
-				/>
+        <Image
+          src="/logodolphin.png"
+          alt="logo"
+          height={60}
+          width={60}
+          className="rounded-full"
+          priority
+        />
       </Link>
     </header>
   );
