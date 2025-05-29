c5ab4c5f45cd9f841ea0448b433f0562cf90624f
tamir.rustamov <timorust1@gmail.com>
2025-05-29 10:25:43 +0300

    server and prisma generate

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import React from "react";
 import { Button } from "./ui/button";
 import DarkModeToggle from "./DarkModeToggle";
 import LogOutButton from "./LogoutButton";
-import { getUser } from "@/auth/sever";
+import { getUser } from "@/auth/server";
 
 async function Header() {
   const user = await getUser();
